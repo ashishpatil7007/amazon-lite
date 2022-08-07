976aec28633ac40f53ea40fb8f64805d2f1e44ef
Ashish Patil <ashishpatil7007@gmail.com>
2022-08-07 20:28:36 +0530

    Project Added

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+app= Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/cart")
+def cart():
+    return render_template("cart.html")
+
+@app.route("/checkout")
+def checkout():
+    return render_template("checkout.html")
+
+app.run(debug=True)
+
