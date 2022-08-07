ee6f03f0602ea8ebce8570683d3e61dc6f5f1941
Ashish Patil <ashishpatil7007@gmail.com>
2022-08-07 20:33:31 +0530

    Updated Code For Port

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import os
 app= Flask(__name__)
 @app.route("/")
 def home():
@@ -16,5 +17,10 @@ def cart():
 def checkout():
     return render_template("checkout.html")
 
-app.run(debug=True)
+if __name__ == '__main__':
+   if 'PORT' in os.environ:
+      port = os.environ['PORT']
+   else:
+      port= 5000
+   app.run(debug=False,host="0.0.0.0",port=port)
 
