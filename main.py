843ca132afe989fab3423ac7a00c73ee84154de6
Ashish Patil <ashishpatil7007@gmail.com>
2022-09-07 09:52:46 +0530

    Updated Code

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 @app.route("/cart")
 def cart():
     return render_template("cart.html")
@@ -17,10 +21,16 @@ def cart():
 def checkout():
     return render_template("checkout.html")
 
+@app.route("/product_details")
+def product_details():
+    return render_template("product_details.html")
+
+
+
 if __name__ == '__main__':
    if 'PORT' in os.environ:
       port = os.environ['PORT']
    else:
       port= 5000
-   app.run(debug=False,host="0.0.0.0",port=port)
+   app.run(debug=True,host="0.0.0.0",port=port)
 
